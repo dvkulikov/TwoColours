17671e3b85085acd52f5215ac259a9a0fd21e674
dv.kulikov <dv.kulikov@mail.ru>
2020-08-21 23:04:31 +0300

    Initial Commit

---
+++ TwoColours/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwoColours
+//
+//  Created by 1 on 21.08.2020.
+//  Copyright © 2020 Denis Kulikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
